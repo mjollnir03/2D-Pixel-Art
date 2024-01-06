f66760ada09a68270f957a3f6eeddd203b5c05b7
Ellmaer <ellmaer2003@gmail.com>
2024-01-06 13:56:30 -0800

    Major changes made to the left panel, will continue to add the btn-on class for all the buttons in the .panel div

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 //If you import any files/modules, the main.js file will not run
 //Do not define this file as a module
 
+//PUBLIC VARIABLES
+const rangeSlider = document.getElementById('range-slider');
+const progressBar = document.getElementById('progress-bar');
 
-let rangeSlider = document.getElementById('range-slider');
-let progressBar = document.getElementById('progress-bar');
+const rangeValue = document.querySelectorAll('.range-value');
+
+// const 
 
 // Function to update progress bar width based on range slider value
 function updateProgressBar() {
     let value = rangeSlider.value;
+    
     progressBar.style.width = (value / 60) * 100 + '%';
+    //Call update rangeValue
+    updateRangeValue(value)
+}
+
+function updateRangeValue(value) {
+    for(let i = 0; i < rangeValue.length; i++)
+    {
+        rangeValue[i].textContent = value; 
+    }
 }
 
 // Add an event listener to detect changes in the range slider value
