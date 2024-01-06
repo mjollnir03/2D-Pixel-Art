8d5ceeb54314400adf6a256688ea16552e22b532
Ellmaer <ellmaer2003@gmail.com>
2024-01-05 16:57:15 -0800

    Major initial updates made all needed files have been added will add on accordingly as needed will continue to setup the HTML blueprint and continue to fix minor bugs

---
+++ src/main.js
@@ -0,0 +1,12 @@
+//If you import any files/modules, the main.js file will not run
+//Do not define this file as a module
+
+
+console.log("Hello World");
+
+
+const container = 
+function cool()
+{
+    return 3;
+}
