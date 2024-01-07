0000c5b9385db7fc40e6c6309b4f7d53863c53ce
Ellmaer <ellmaer2003@gmail.com>
2024-01-06 16:24:53 -0800

    Panel Design in done, now I will attempt to add the grid for the grid container

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@
 //Do not define this file as a module
 
 //PUBLIC VARIABLES
+const colorSelect = document.getElementById('color-select');
+const bgColorSelect = document.getElementById('bg-color-select');
+
 let gridValue;
+let penColor;
+let bgPenColor;
 const rangeSlider = document.getElementById('range-slider');
 const progressBar = document.getElementById('progress-bar');
 
@@ -10,7 +15,8 @@ const rangeValue = document.querySelectorAll('.range-value');
 
 const buttons = document.getElementsByTagName('button');
 
-const clearButton = document.getElementById('clear-grid'); //special case
+
+
 
 // Function to update progress bar width based on range slider value
 function updateProgressBar() {
@@ -28,27 +34,48 @@ function updateRangeValue(value) {
     }
 }
 
-//ALSO HAVE TO ADD THAT WHEN ANOTHER BUTTON IS CLICKED, TOO UN TOGGLE THE OTHER BUTTON
-
-function toggleOn() {
-    //first check if any others are on
-    
-}
-
-function clearGrid() {
-
-}
+function checkButtons(buttonId) {
+    for (let i = 0; i < 4; i++) {
+      if (buttons[i] === buttonId) {
+        // Toggle the class for the clicked button
+        buttons[i].classList.toggle('btn-on');
+      } else {
+        // Remove the class for other buttons
+        buttons[i].classList.remove('btn-on');
+      }
+    }
+  }
 
 
 // EVENT LISTENERS
-// Add an event listener to detect changes in the range slider value
-rangeSlider.addEventListener('input', updateProgressBar);
-for(let i = 0; i < buttons.length; i++)
-{
-    buttons[i].addEventListener('click', () => {buttons[i].classList.toggle('btn-on')});
-}
 
-clearButton.addEventListener('click', );
+colorSelect.addEventListener('input', () => {
+    penColor = colorSelect.value;
+});
+
+bgColorSelect.addEventListener('input', () => {
+    bgPenColor = bgColorSelect.value;
+});
+
+rangeSlider.addEventListener('input', updateProgressBar()); //update slider 1 - 60
+
+//BUTTONS {brute force}
+for(let i = 0; i < 4; i++)
+{
+    buttons[i].addEventListener('click', () => {
+        checkButtons(buttons[i]);
+    });
+}
+buttons[4].addEventListener('click', () => { //toggle grid
+    buttons[4].classList.toggle('btn-on');
+    //need to add toggle grid function later
+})
+buttons[5].addEventListener('click', () => { //clear grid
+    buttons[5].classList.toggle('btn-on');
+    //need to add clear grid function later on
+})
+
+
 
 
 
