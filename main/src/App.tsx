6714d58922a2c5624c405cdcb857de517f0ed973
Ellmaer Ranjber <ellmaer2003@gmail.com>
2025-10-04 17:49:44 -0700

    save changes

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,12 +1,23 @@
 import "./styles/App.css";
 import Header from "./components/Header";
+// import
 
 function App() {
   return (
     <>
+      {/* Webpage */}
       <div className="flex flex-col h-screen">
         {/* Header */}
         <Header />
+        {/* Main Content */}
+        <div>
+          {/* Left Panel */}
+
+          {/* Canvas */}
+
+          {/* Right Panel */}
+          
+        </div>
       </div>
     </>
   );
