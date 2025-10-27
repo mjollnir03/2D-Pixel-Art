2a7401db5b65a216654d2d14f3f194394827d9e7
Ellmaer <ellmaer2003@gmail.com>
2025-10-26 19:28:24 -0700

    Update Canvas.tsx

--- main/src/components/Canvas.tsx
+++ main/src/components/Canvas.tsx
@@ -1,5 +1,13 @@
-// components/Canvas.tsx
+import { useRef } from "react";
+
 export default function Canvas() {
+
+  const {innerWidth, innerHeight} = window;
+
+  let canvasRef = useRef<HTMLCanvasElement | null>(null);
+  
+  const ctx = canvasRef.current?.getContext('2d');
+
   return (
     <div className="flex justify-center items-center w-full">
       <canvas
